dd072bc92d4f74388d14563ca689542021bfd82b
Alexander Vogel <alexander.vogel@prozesskraft.de>
2017-10-31 19:27:32 +0100

    edit labels

---
+++ src/labels.h
@@ -0,0 +1,29 @@
+#ifndef LABELS_H
+#define LABELS_H
+
+
+// Labels for Sensor Fusion
+enum SF
+{
+	SF_id,
+	SF_x,
+	SF_y,
+	SF_vx,
+	SF_vy,
+	SF_s,
+	SF_d,
+};
+
+// Labels for Ego Car
+enum Ego
+{
+    Ego_x,
+  	Ego_y, 
+  	Ego_s,
+  	Ego_d,
+  	Ego_yaw,
+  	Ego_speed,
+};
+
+
+#endif
